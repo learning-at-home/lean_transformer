311305f1e67ba9d3b73fe4e9c0d5da66a54be6bd
justheuristic <justheuristic@gmail.com>
2022-01-16 11:22:43 +0300

    merge test_adapter.py into test_linear.py

--- tests/test_linear.py
+++ tests/test_linear.py
@@ -1,9 +1,49 @@
+from typing import Optional
+
 import pytest
 import torch
 from torch import nn as nn
 from torch.nn import functional as F
 
-from lib import SemiSharedLinear, SharedMatrix
+from lib.modules.linear import SemiSharedLinear, SharedMatrix, _SemiSharedLinear
+
+
+def adapted_linear_naive(
+    input: torch.Tensor,
+    matrix: torch.Tensor,
+    adapter_first: torch.Tensor,
+    adapter_second: torch.Tensor,
+    bias: Optional[torch.Tensor],
+):
+    adapter_hid = F.linear(input, adapter_first)
+    shared_out = F.linear(input, matrix, bias)
+    return adapter_hid @ adapter_second.t() + shared_out
+
+
+def test_semishared_linear_naive():
+    input = torch.randn(3, 15, 1024, requires_grad=True)
+    weight = torch.randn(4096, 1024, requires_grad=True)
+    adapter_first = torch.randn(64, 1024, requires_grad=True)
+    adapter_second = torch.randn(4096, 64, requires_grad=True)
+    bias = torch.randn(4096, requires_grad=True)
+    random_dir = torch.randn(3, 15, 4096)
+
+    out_ours = _SemiSharedLinear.apply(input, weight, bias, adapter_first, adapter_second, None, None)
+    torch.sum(out_ours * random_dir).backward()
+    grads_ours = tuple(tensor.grad.clone() for tensor in (input, weight, adapter_first, adapter_second, bias))
+
+    for tensor in (input, weight, adapter_first, adapter_second, bias):
+        tensor.grad = None
+
+    out_ref = adapted_linear_naive(input, weight, adapter_first, adapter_second, bias)
+    torch.sum(out_ref * random_dir).backward()
+
+    grads_ref = tuple(tensor.grad.clone() for tensor in (input, weight, adapter_first, adapter_second, bias))
+
+    assert torch.allclose(out_ours, out_ref)
+
+    for grad_ours, grad_ref in zip(grads_ours, grads_ref):
+        assert torch.allclose(grad_ours, grad_ref)
 
 
 class ReferenceLinear(nn.Module):
