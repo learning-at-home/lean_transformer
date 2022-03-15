03a32e61f34940af1e54bb70c15602302c85261d
justheuristic <justheuristic@gmail.com>
2022-03-15 09:13:10 +0300

    internal version logic: v0.1.0 = sahajbert-v2 v0.2.0 = CALM and RuBERT v0.3.0 = this

--- lean_transformer/__init__.py
+++ lean_transformer/__init__.py
@@ -4,3 +4,5 @@ from .linear import *
 from .pixelfly import *
 from .rotary import *
 from .sequence import *
+
+__version__ = "0.3.0"
--- setup.py
+++ setup.py
@@ -1,24 +1,17 @@
 import codecs
-import glob
-import hashlib
 import os
 import re
-import shlex
-import subprocess
-import tarfile
-import tempfile
-import urllib.request
 
 from pkg_resources import parse_requirements, parse_version
 from setuptools import find_packages, setup
-from setuptools.command.build_py import build_py
-from setuptools.command.develop import develop
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 with open("requirements.txt") as requirements_file:
     install_requires = list(map(str, parse_requirements(requirements_file)))
 
 # loading version from setup.py
-with codecs.open(os.path.join(here, "hivemind/__init__.py"), encoding="utf-8") as init_file:
+with codecs.open(os.path.join(here, "lean_transformer/__init__.py"), encoding="utf-8") as init_file:
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", init_file.read(), re.M)
     version_string = version_match.group(1)
 
@@ -33,24 +26,19 @@ with open("requirements-docs.txt") as docs_requirements_file:
 extras["all"] = extras["dev"] + extras["docs"]
 
 setup(
-    name="hivemind",
+    name="lean_transformer",
     version=version_string,
-    cmdclass={"build_py": BuildPy, "develop": Develop},
-    description="Decentralized deep learning in PyTorch",
-    long_description="Decentralized deep learning in PyTorch. Built to train models on thousands of volunteers "
-    "across the world.",
+    description="PyTorch transformers that don't hog your GPU memory.",
+    long_description="Memory-efficient transformers with optional sparsity, reversible layers, checkpoints, etc.",
     author="Learning@home & contributors",
     author_email="hivemind-team@hotmail.com",
-    url="https://github.com/learning-at-home/hivemind",
+    url="https://github.com/learning-at-home/lean_transformer",
     packages=find_packages(exclude=["tests"]),
-    package_data={"hivemind": ["proto/*", "hivemind_cli/*"]},
-    include_package_data=True,
     license="MIT",
-    setup_requires=["grpcio-tools"],
     install_requires=install_requires,
     extras_require=extras,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 4 - Alpha",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
@@ -65,11 +53,5 @@ setup(
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    entry_points={
-        "console_scripts": [
-            "hivemind-server = hivemind.hivemind_cli.run_server:main",
-        ]
-    },
-    # What does your project relate to?
-    keywords="pytorch, deep learning, machine learning, gpu, distributed computing, volunteer computing, dht",
+    keywords="pytorch, deep learning, machine learning, gpu, efficient training, efficient inference",
 )
